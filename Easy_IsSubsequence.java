3d419057cf646ef5959180f1d617a6be42973193
jcamacho <jcamacho@macbook-pro.mynetworksettings.com>
2023-02-21 20:27:25 -0500

    Merge Two Sorted Lists Solution and Is Subsequence space optimization

--- Easy_IsSubsequence.java
+++ Easy_IsSubsequence.java
@@ -1,16 +1,12 @@
 public class Easy_IsSubsequence {
     public boolean isSubsequence(String s, String t){
-        if(s.length() > t.length()) return false;
-        if(s.isEmpty()) return true;
         int count = 0;
         for(int i = 0; i < t.length() && count < s.length(); i++){
             if(s.charAt(count) == t.charAt(i)){
                 count++;
-                if(count == s.length()) return true;
             }
         }
-        if(count != s.length()) return false;
-        return true;
+        return count == s.length();
     }
     public static void main(String[] args){
         Easy_IsSubsequence subsequence = new Easy_IsSubsequence();
