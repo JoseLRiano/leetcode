f5defd952f3bfab48776d601fb0059ce90ca0451
JoseLRiano <joselriano10@gmail.com>
2023-06-26 18:31:46 -0400

    Java Product Except Self using two pointers

--- productOfArrayExceptSelf.java
+++ productOfArrayExceptSelf.java
@@ -1,5 +1,15 @@
 class Solution{
     public int[] productExceptSelf(int[] nums){
-        
+        int right = 1;
+        int[] solution= new int[nums.length];
+        solution[0] = 1;
+        for(int i = 1; i < nums.length; i++){
+            solution[i] = nums[i - 1] * solution[i - 1];
+        }
+        for(int j = nums.length - 1; j >= 0; j--){
+            solution[j] = solution[j] * right;
+            right = right * nums[j];
+        }
+        return solution;
     }
 }
