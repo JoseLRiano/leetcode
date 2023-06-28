57cc22c9457dc83eb8f818c885e1ceb6b4ba15c8
JoseLRiano <joselriano10@gmail.com>
2023-06-28 15:55:10 -0400

    Product Except Self JavaScript

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -1,3 +1,12 @@
-var productExceptSelf = funtion(nums) {
-    
+var productExceptSelf = function(nums) {
+    let right = 1;
+    let solution = [0];
+    nums.forEach((item, index) => {
+        item > 0 && (solution[index] = nums[index -1] * solution[index -1])
+    });
+    for(let i = nums.length -1; i >= 0; i--){
+        solution[i] = solution[i] * right;
+        right = right * nums[i];
+    }
+    return solution;
 }
