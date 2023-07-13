12f507c531272b84bb90b88721e4c38a135b3c75
JoseLRiano <joselriano10@gmail.com>
2023-07-13 16:49:35 -0400

    using the Two pointer approach - Java

--- maxNumbersOfK-SumPairs.java
+++ maxNumbersOfK-SumPairs.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 class maxNumbersOfKSumParis {
@@ -5,13 +6,27 @@ class maxNumbersOfKSumParis {
         HashMap<Integer, Integer> map = new HashMap<>();
         int count= 0;
         for(int item : nums){
-            if(map.containsKey(k - item) && map.get(k - item) > 0){
+            if(map.getOrDefault(k - item, 0) > 0){
                 count++;
                 map.put(k - item, map.get(k - item)-1);
             }else{
                 map.put(item, map.getOrDefault(item, 0) +1);
             }
         }
-        return count++;
+        return count;
+
+        // Two Pointer Approach 
+        // Arrays.sort(nums);
+        // int left = 0;
+        // int right = nums.length -1;
+        // int count = 0;
+        // while(left < right){
+        //     if(nums[left] + nums[right] == k){ count++; left++; right--;}
+        //     else if(nums[left] + nums[right] < k) left++;
+        //     else right--;
+        // }
+        // return count;
+
+
     }
 }
